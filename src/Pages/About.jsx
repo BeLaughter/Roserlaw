f3a0d09dd30e75575ae388cae29699d731ccea00
Tufem Gift Items <Laughteriremide@gmail.com>
2025-03-12 16:54:45 +0100

    commit changes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Aboutus from "../Components/Aboutus";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <Aboutus />
+    </div>
+  );
 };
 
 export default About;
