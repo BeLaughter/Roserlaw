5cf4441355a9db0b0652369c1fa96d3070346227
Tufem Gift Items <Laughteriremide@gmail.com>
2025-03-17 02:22:03 +0100

    firebase updates

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure output directory is set correctly
+  },
   server: {
-    historyApiFallback: true, // Ensures React Router handles all routes
+    historyApiFallback: true, // Ensures dev server handles routing properly
   },
 });
